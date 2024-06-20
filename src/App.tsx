d1ff361a5bac4b16a8dbe7defd2f58cc0f082fdf
Mateie <mateie@email.com>
2024-06-20 03:58:01 -0700

    added a check for server pulse

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import { Home, Login, NotFound, Signup } from "./pages";
+import { useQuery } from "@apollo/client";
+import { PulseCheck } from "./gql/general";
+import { ErrorMessage } from "./components";
 
 const App = () => {
+    const { data: { pulse } = {} } = useQuery(PulseCheck);
+
+    if (!pulse)
+        return (
+            <ErrorMessage
+                message="😢 Server is down 😢"
+                subtext={[
+                    "Please try again later.",
+                    "We apologize for the inconvenience.",
+                    "You can also contact developers for more information."
+                ]}
+            />
+        );
+
     return (
         <div className="block">
             <Routes>
